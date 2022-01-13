fb51c73e07fe08deab808a24ef52faca181ff650
janainamai <janainamai@hotmail.com>
2022-01-13 00:23:44 -0300

    Implementing Movie

--- src/main/java/br/com/movie/service/MovieService.java
+++ src/main/java/br/com/movie/service/MovieService.java
@@ -31,15 +31,15 @@ public class MovieService {
     }
 
     public Movie save(Movie movie) {
-        if (!valid(movie)) {
+        if (!validToSave(movie)) {
             throw new BadRequestException("A movie with this name, language and type already exists");
         }
         return movieRepository.save(movie);
     }
 
     public Movie replace(Movie movie) {
-        if(movieRepository.existsById(movie.getId())) {
-            return this.save(movie);
+        if (movieRepository.existsById(movie.getId()) && validToReplace(movie)) {
+            return movieRepository.save(movie);
         } else {
             throw new BadRequestException(MOVIE_NOT_FOUND);
         }
@@ -51,13 +51,34 @@ public class MovieService {
      * @param movie Movie to be validated
      * @return false if already exists and true if not
      */
-    private boolean valid(Movie movie) {
+    private boolean validToSave(Movie movie) {
         List<Movie> list = movieRepository.findByTitleIgnoreCase(movie.getTitle());
 
         if (!list.isEmpty()) {
             for (Movie mov : list) {
                 if (movie.getLanguage().equals(mov.getLanguage())
                         && movie.getMovieType().equals(mov.getMovieType())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Validates if a movie with this title, language and type already exists.
+     *
+     * @param movie Movie to be validated
+     * @return false if already exists and true if not
+     */
+    private boolean validToReplace(Movie movie) {
+        List<Movie> list = movieRepository.findByTitleIgnoreCase(movie.getTitle());
+
+        if (!list.isEmpty()) {
+            for (Movie data : list) {
+                if (movie.getLanguage().equals(data.getLanguage())
+                        && movie.getMovieType().equals(data.getMovieType())
+                        && movie.getId() != data.getId()) {
                     return false;
                 }
             }
