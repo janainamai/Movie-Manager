82a61b1ec5f6c242465b170f26ddb6b0c7494ace
janainamai <janainamai@hotmail.com>
2022-01-12 22:27:11 -0300

    Implementing movie

---
+++ src/main/java/br/com/movie/model/dto/MoviePost.java
@@ -0,0 +1,49 @@
+package br.com.movie.model.dto;
+
+import br.com.movie.model.Category;
+import br.com.movie.model.Language;
+import br.com.movie.model.Movie;
+import br.com.movie.model.enums.MovieType;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+public class MoviePost implements ConvertDTO<Movie>{
+
+    @NotNull(message = "Title cannot be null")
+    private String title;
+
+    @NotNull(message = "Synopsis cannot be null")
+    private String synopsis;
+
+    @NotEmpty(message = "Category cannot be null")
+    private List<Category> category;
+
+    @NotNull(message = "Age group cannot be null")
+    private int ageGroup;
+
+    @NotNull(message = "Language cannot be null")
+    private Language language;
+
+    @NotNull(message = "Duration cannot be null")
+    private int duration;
+
+    @NotNull(message = "Movie type cannot be null")
+    private MovieType movieType;
+
+    @Override
+    public Movie toEntity() {
+        return Movie.builder()
+                .title(this.title)
+                .synopsis(this.synopsis)
+                .category(this.category)
+                .ageGroup(this.ageGroup)
+                .language(this.language)
+                .duration(this.duration)
+                .movieType(this.movieType)
+                .build();
+    }
+}
---
+++ src/main/java/br/com/movie/repository/MovieRepository.java
@@ -0,0 +1,14 @@
+package br.com.movie.repository;
+
+import br.com.movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    Optional<Movie> findByTitle(String title);
+
+}
