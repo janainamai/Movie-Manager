b9bcf1f739f4eb474dc44c568a4e84e8ee447519
janainamai <janainamai@hotmail.com>
2022-01-30 15:00:55 -0300

    Implementing day of week discount

---
+++ src/main/java/br/com/movie/model/dto/DayOfWeekDiscountPost.java
@@ -0,0 +1,30 @@
+package br.com.movie.model.dto;
+
+import br.com.movie.model.DayOfWeekDiscount;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class DayOfWeekDiscountPost implements ConvertDTO<DayOfWeekDiscount> {
+
+    @NotNull(message = "dayOfWeek cannot be null")
+    @NotEmpty(message = "dayOfWeek cannot be empty")
+    private String dayOfWeek;
+
+    @NotNull(message = "isActive cannot be null")
+    private Boolean isActive;
+
+    @NotNull(message = "percentage cannot be null")
+    private Double percentage;
+
+    @Override
+    public DayOfWeekDiscount toEntity() {
+        return DayOfWeekDiscount.builder()
+                .dayOfWeek(this.dayOfWeek)
+                .isActive(this.isActive)
+                .percentage(this.percentage)
+                .build();
+    }
+}
