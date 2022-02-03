70bc36a25251301e4f4d9ae21750ae799c3f1492
janainamai <janainamai@hotmail.com>
2022-02-02 22:41:41 -0300

    Implementing buildRoom

--- src/main/java/br/com/movie/model/dto/BuildRoom.java
+++ src/main/java/br/com/movie/model/dto/BuildRoom.java
@@ -3,12 +3,17 @@ package br.com.movie.model.dto;
 import br.com.movie.model.Room;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class BuildRoom {
 
+    @NotNull( message = "Room cannot be null")
     private Room room;
 
+    @NotNull(message = "numberOfRows cannot be null")
     private Integer numberOfRows;
 
+    @NotNull(message = "armchairPerRow cannot be null")
     private Integer armchairPerRow;
 }
