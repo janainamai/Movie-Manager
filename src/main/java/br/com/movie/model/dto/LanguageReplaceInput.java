6b892a452e2de2e5fb0d6080779e2438ca20d032
janainamai <janainamai@hotmail.com>
2022-03-03 22:42:24 -0300

    Implementing language

--- src/main/java/br/com/movie/model/dto/LanguageReplaceInput.java
+++ src/main/java/br/com/movie/model/dto/LanguageReplaceInput.java
@@ -4,9 +4,13 @@ import br.com.movie.model.Language;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
-public class LanguagePost implements ConvertDTO<Language> {
+public class LanguageReplaceInput implements ConvertDTO<Language> {
+
+    @NotNull(message = "Identifier cannot be null")
+    private int id;
 
     @NotEmpty(message = "Description cannot be null or empty")
     private String description;
@@ -14,6 +18,7 @@ public class LanguagePost implements ConvertDTO<Language> {
     @Override
     public Language toEntity() {
         return Language.builder()
+                .id(this.id)
                 .description(this.description)
                 .build();
     }
--- src/main/java/br/com/movie/repository/LanguageRepository.java
+++ src/main/java/br/com/movie/repository/LanguageRepository.java
@@ -4,6 +4,7 @@ import br.com.movie.model.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,5 @@ public interface LanguageRepository extends JpaRepository<Language, Integer> {
 
     Optional<Language> findByDescriptionIgnoreCase(String description);
 
+    Optional<List<Language>> findByDescriptionContainingIgnoreCase(String description);
 }
